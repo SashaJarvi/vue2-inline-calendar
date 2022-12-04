83fb07fcca3f360b7a33a836a5fa27a4772c5949
SashaJarvi <alexandrermakov2@gmail.com>
2022-12-04 21:33:37 +0400

    extended package.json, added deploy.sh, added info about sponsorship to readme

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build:gh
+
+# navigate into the build output directory
+cd dist-gh
+
+git init
+git checkout main || git checkout -b main
+git add -A
+git commit --allow-empty -m 'Deploy to Github Pages'
+
+# we are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/SashaJarvi/vue2-inline-calendar.git main:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/vue2-inline-calendar/" : "/",
   transpileDependencies: true,
   css: {
     extract: false,
